2f97e2eea7e9667412820a8b5eac9584c5c43ada
Andrii Yarmak <andreyyarmak777@gmail.com>
2024-07-24 23:36:42 +0300

    fixed scrolltop

--- src/Component/ScrollToTop/ScrollToTop.tsx
+++ src/Component/ScrollToTop/ScrollToTop.tsx
@@ -1,25 +1,16 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const ScrollToTop = () => {
+const ScrollToTop: React.FC = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const scrollToTop = () => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-
-    scrollToTop();
-
-    const handleHashChange = () => {
-      scrollToTop();
-    };
-
-    window.addEventListener('hashchange', handleHashChange);
-
-    return () => {
-      window.removeEventListener('hashchange', handleHashChange);
-    };
+    if (window.scrollTo) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth', 
+      });
+    }
   }, [pathname]);
 
   return null;
--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import ScrollToTop from './Component/ScrollToTop/ScrollToTop';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
